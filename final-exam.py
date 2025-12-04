b71d06576c1a7021dc9c7b0843ab0c8d3af5baa1
Sleepycoffeejune <wiran.wa@ku.th>
2025-12-04 16:08:40 +0700

    2nd commit

---
+++ final-exam.py
@@ -0,0 +1,74 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def introduce(self):
+        print(f"Hi, I'm {self.name}.")
+    
+class Customer(Person):
+    def __init__(self, name, address):
+        super().__init__(name)
+        self.name = name
+        self.address = address
+        
+        
+    def place_order(self, item):
+        new_order = DeliveryOrder(self.name, item, status="preparing")
+        return new_order
+
+class Driver(Person):
+    def __init__(self, name, vehicle):
+        super().__init__(name)
+        self.name = name
+        self.vehicle = vehicle
+    
+
+    def deliver(self, order):
+        order.status == "delivered"
+        print(f"{self.name} is delivering {order.item} to {order.name} using {self.vehicle}.")
+    
+
+class DeliveryOrder:
+    def __init__(self, customer, item, status):
+        self.customer = customer
+        self.item = item
+        self.status = "preparing"
+        self.driver = driver
+    
+
+    def assign_driver(driver):
+        driver.deliver()
+    
+
+    def summary(self):
+        print("Order Summary:")
+        print(f"Item: {self.item}\nCustomer: {self.customer.name}\nStatus: {self.status}\nDriver: {self.driver.name}")
+
+        
+
+
+customer1 = Customer("Alice", "Address")
+customer2 = Customer("Bob", "Address")
+driver = Driver("David", "motorcycle")
+
+customer1.introduce()
+customer2.introduce()
+driver.introduce()
+
+order1 = customer1.place_order("Laptop")
+order2 = customer2.place_order("Headphones")
+
+order1.summary()
+order2.summary()
+
+driver.deliver(order1)
+driver.deliver(order2)
+
+
+print("Final Status:")
+print(f"Order for {order1.item} → {order1.status}")
+print(f"Order for {order2.item} → {order2.status}")
+
+
+
+
